5c11ea430ac67a3ff33757bbf9cdd36ccb74b7b0
zhouxin <zhouxin@bixin.cn>
2021-06-03 19:20:57 +0800

    Splash module

---
+++ Module_Splash/module_splash/src/main/java/com/zx/module_splash/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.zx.module_splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.alibaba.android.arouter.launcher.ARouter
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    fun skipHomePage(view: View) {
+        ARouter.getInstance().build("/home/entry").navigation()
+        finish()
+    }
+
+    fun skipLoginPage(view: View) {
+        ARouter.getInstance().build("/login/entry").navigation()
+        finish()
+    }
+}
